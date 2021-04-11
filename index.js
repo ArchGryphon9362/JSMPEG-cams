39e3a5dbeb551890a57fa46a8325dbe20f0d2f89
Lex Nastin <lexnastin@gmail.com>
2021-04-11 22:12:57 +0000

    Should be fine now Added modifications for .env and environment variables for docker :)

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+require('dotenv').config()
+
 Stream = require('node-rtsp-stream');
 stream = new Stream({
 	name: 'name',
-	streamUrl: 'rtsp://admin:@192.168.68.110/user=admin&password=&channel=1&stream=0.sdp?',
+	streamUrl: process.env.CAM_RTSP_URL,
 	wsPort: 9999,
 	ffmpegOptions: { // options ffmpeg flags
 		'-stats': '', // an option with no neccessary value uses a blank string
